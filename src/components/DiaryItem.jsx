cfbc4b85c9edd818b344535dce9328733c86ca42
Stilllee <orodae@gmail.com>
2023-08-22 23:11:15 +0900

    홈 - 페이지 이동 기능 추가

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,14 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
+
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || ""; // 배포 환경에서는 PUBLIC_URL이 설정되지 않으므로, 설정되지 않았을 때를 대비해 기본값을 설정
 
   const strDate = new Date(parseInt(date)).toLocaleDateString(); // 타임스탬프를 Date 객체로 변환한 후, 날짜 형식에 맞게 변환
 
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <div className="DiaryItem">
       <div
+        onClick={goDetail}
         className={[
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
@@ -19,12 +31,12 @@ const DiaryItem = ({ id, emotion, content, date }) => {
           alt=""
         />
       </div>
-      <div className="info_wrapper">
+      <div onClick={goDetail} className="info_wrapper">
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
       <div className="btn_wrapper">
-        <MyButton text={"수정하기"} />
+        <MyButton onClick={goEdit} text={"수정하기"} />
       </div>
     </div>
   );
