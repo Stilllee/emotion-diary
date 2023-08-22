8834da0b4df8e405747467caf6a675081613c917
Stilllee <orodae@gmail.com>
2023-08-22 16:07:18 +0900

    홈 - Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`; // JavaScript의 Date 객체는 월을 0부터 시작하므로 1을 더해줌
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate()) // 현재 날짜에서 월만 1 증가
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate()) // 현재 날짜에서 월만 1 감소
+    );
+  };
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>이곳은 홈 입니다.</p>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
     </div>
   );
 };
