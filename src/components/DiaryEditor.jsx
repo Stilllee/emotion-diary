548d1cd6fc55291da3c963ec4deef696d1217d0a
Stilllee <orodae@gmail.com>
2024-01-13 17:23:41 +0900

    감정을 유틸로 분리

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -5,37 +5,10 @@ import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
 import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
 const env = process.env; // 현재 실행 환경의 환경 변수를 env라는 상수에 할당
 env.PUBLIC_URL = env.PUBLIC_URL || ""; // env.PUBLIC_URL이 존재하면 그 값을 사용하고, 존재하지 않으면 빈 문자열("")을 할당
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "완전 좋음",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "좋음",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁨",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "끔찍함",
-  },
-];
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
