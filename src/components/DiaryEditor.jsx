e0a6e564721f129ecc72cf449d6ff55b512e77ef
Stilllee <orodae@gmail.com>
2024-01-13 19:16:56 +0900

    일기 수정 중에 발생하는 감정아이템 렌더링 최적화

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useContext, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { DiaryDispatchContext } from "../App";
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
@@ -18,9 +18,9 @@ const DiaryEditor = ({ isEdit, originData }) => {
 
   const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
 
-  const handleClickEmote = (emotion) => {
+  const handleClickEmote = useCallback((emotion) => {
     setEmotion(emotion);
-  };
+  }, []);
 
   const navigate = useNavigate();
 
