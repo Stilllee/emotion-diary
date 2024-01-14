19cbe813e2dbcc3dc863af3ba2cd278b1772a55f
Stilllee <orodae@gmail.com>
2024-01-14 23:40:53 +0900

    인덱스 페이지의 타이틀 매핑이 제대로 되지않는 에러 처리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,11 @@ const Home = () => {
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`; // JavaScript의 Date 객체는 월을 0부터 시작하므로 1을 더해줌
 
   useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장`;
+  }, []);
+
+  useEffect(() => {
     if (diaryList.length >= 1) {
       const firstDay = new Date(
         curDate.getFullYear(),
