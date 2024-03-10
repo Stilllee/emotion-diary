d20f683fae63f98d0679e943bc11231e7ff78727
Stilllee <orodae@gmail.com>
2024-03-10 22:21:40 +0900

    ✨ Home 페이지 기능 구현 - DiaryList
    - 버튼 클릭시 일기 작성 페이지로 이동
    - 최신순 - 오래된순 정렬 기능

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,22 +1,53 @@
 import Button, { ButtonType } from "components/Button";
 import DiaryItem from "components/DiaryItem";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { DiaryType } from "types/diary-types";
 
-export default function DiaryList({ data }) {
+interface DiaryListProps {
+  data: DiaryType[];
+}
+
+export default function DiaryList({ data }: DiaryListProps) {
+  const nav = useNavigate();
+
+  const [sortType, setSortType] = useState<"latest" | "oldest">("latest");
+
+  const onChangeSortType = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortType(e.target.value as "latest" | "oldest");
+  };
+
+  const getSortedDate = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedDate();
+
   return (
     <div>
       <div className="my-5 flex gap-3">
-        <select className="bg-btnLight rounded-md py-3 px-5 cursor-pointer">
+        <select
+          onChange={onChangeSortType}
+          className="bg-btnLight rounded-md py-3 px-5 cursor-pointer"
+        >
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된 순</option>
         </select>
         <Button
+          onClick={() => nav("/new")}
           className="flex-1"
           text={"새 일기 쓰기"}
           type={ButtonType.POSITIVE}
         />
       </div>
       <div>
-        {data.map((item) => (
+        {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>
