b305a8554785a5f36f4df2e187eddde73b75e9ef
Stilllee <orodae@gmail.com>
2024-03-10 21:42:20 +0900

    ✨ Home 페이지 기능 구현 - 날짜, 기간
    - Header에 현재 날짜 표기
    - Header 전월, 익월 변경 기능
    - 당월 1일 ~ 말일까지만 렌더링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,54 @@
 import Button from "components/Button";
 import DiaryList from "components/DiaryList";
 import Header from "components/Header";
+import { DiaryStateContext } from "context/diary-context";
+import { useContext, useState } from "react";
+import { DiaryType } from "types/diary-types";
+
+const getMonthlyData = (pivotDate: Date, data: DiaryType[]) => {
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime();
+
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime();
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+};
 
 export default function Home() {
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMonthlyData(pivotDate, data);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <>
       <Header
-        title={"2024년 3월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
       />
-      <DiaryList />
+      <DiaryList data={monthlyData} />
     </>
   );
 }
