3089fe24965ca6bd6a7dea78d367f372b26b3571
Stilllee <orodae@gmail.com>
2024-03-15 00:19:46 +0900

    💄 Footer 스타일 코드 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const { isDarkMode, toggleDarkMode } = useContext(ThemeContext);
 
   return (
-    <div className="fixed bottom-0 left-0 flex justify-center w-full py-4 z-10 bg-white dark:bg-mainDark">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 flex justify-center max-w-screen-sm w-full py-4 z-10 bg-white dark:bg-mainDark">
       <button
         className="text-2xl dark:text-white transition-all transit hover:scale-110 rounded-full p-1 dark:hover:bg-btnDark hover:bg-btnLight"
         onClick={toggleDarkMode}
